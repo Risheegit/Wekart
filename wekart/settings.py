e862d8c30c55ee53045e8dd2ec7b19ef733d8c13
rishee <rishee.rb@gmail.com>
2023-06-10 21:26:58 +0530

    changesiteid

--- wekart/settings.py
+++ wekart/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # core/settings.py
 
-SITE_ID = 2
+SITE_ID = 3
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
